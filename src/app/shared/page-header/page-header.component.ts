43a6d46e1d60207e0b3ec6654ab581a982ad2f0f
Joao Rocha <jsfrocha@gmail.com>
2020-04-18 21:15:17 +0100

    Homepage Header;

---
+++ src/app/shared/page-header/page-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 't4e-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.scss']
+})
+export class PageHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import {RouterModule} from '@angular/router';
+import { PageHeaderComponent } from './page-header/page-header.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    PageHeaderComponent
   ],
   imports: [
     CommonModule,
