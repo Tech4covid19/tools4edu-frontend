10b4da37317ce957f95177098d584a53f227c79d
Joao Rocha <jsfrocha@gmail.com>
2020-04-22 02:52:26 +0100

    FilterDrawer Overflow & Overlay

--- src/app/shared/filter-drawer/filter-drawer.component.ts
+++ src/app/shared/filter-drawer/filter-drawer.component.ts
@@ -1,19 +1,28 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, Renderer2, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 't4e-filter-drawer',
   templateUrl: './filter-drawer.component.html',
   styleUrls: ['./filter-drawer.component.scss']
 })
-export class FilterDrawerComponent implements OnInit {
+export class FilterDrawerComponent implements OnChanges {
 
   @Input() isOpened: boolean = false;
   @Output() onClose: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
-  ngOnInit(): void {
-
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['isOpened'].currentValue === true) {
+      this.renderer.addClass(document.body, 'overflow-hidden')
+      this.renderer.addClass(document.body, 'overlay')
+      document.querySelector('.t4e-filter-drawer__content').scroll(0,0);
+    } else {
+      this.renderer.removeClass(document.body, 'overflow-hidden')
+      this.renderer.removeClass(document.body, 'overlay')
+    }
   }
 
   handleClose() {
